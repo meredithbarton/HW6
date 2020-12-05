0275a8915749136fad6bd4e75386093613d34dd9
meredithbarton <62954696+meredithbarton@users.noreply.github.com>
2020-12-05 00:57:52 -0500

    fixed host

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs290_yourengrusername',
   password        : 'last-4-digits-of-your-osu-id',
   database        : 'cs290_yourengrusername'
